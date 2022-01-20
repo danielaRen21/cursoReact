b0bb901977718d123d0731bb6d3d8c325ad57146
danielaRen21 <daniela.f.renteria@gmail.com>
2022-01-19 18:05:25 -0600

    useReducer basico

---
+++ 05-hook-app/src/components/08-useReducer/intro-reducer.js
@@ -0,0 +1,26 @@
+
+
+const initialState = [{
+    id: 1,
+    todo: 'comprar leche',
+    done: false
+}]
+
+export const todoReducer = (state = initialState, action) => {
+    if(action?.type === 'agregar'){
+        return [...state, action.payload];
+    }
+  return state;
+};
+let todos = todoReducer();
+const newTodo ={
+    id: 2,
+    todo: 'comprar huevo',
+    done: false
+}
+const agregarTodoAction = {
+    type: 'agregar',
+    payload: newTodo
+}
+todos = todoReducer(todos, agregarTodoAction)
+console.log(todos)
